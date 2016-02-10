f0b9db6bb71b28b049765f0c3b014bca6ff08da9
grang@gmail.com <wA7kY50N>
2016-02-10 06:29:09 +0200

    Fix Open Browser from PN

--- comkabchannel66/src/main/java/com/kab/channel66/ParsePushHandling.java
+++ comkabchannel66/src/main/java/com/kab/channel66/ParsePushHandling.java
@@ -27,64 +27,61 @@ public  class ParsePushHandling extends ParsePushBroadcastReceiver {
 		if(intent.getAction().equalsIgnoreCase("com.parse.push.intent.RECEIVE"))
 		{
 			super.onReceive(context, intent);
-			Log.d("got a push", null);
-		ParseObject message = new ParseObject("messages");
-		String text = (String) intent.getExtras().get(KEY_PUSH_DATA);
-		try {
-			//JSONArray array = new JSONArray(text);
-		
-			JSONObject pushjson = new JSONObject(text);
-			
-			String val = (String) pushjson.get("alert");
-			
-			message.put("text", val);
-			
-			
-			message.put("date",System.currentTimeMillis());
-			
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		message.pinInBackground();
-		context.sendBroadcast(new Intent("newMessage"));
-		}
-		
-		if(intent.getAction().equalsIgnoreCase("com.parse.push.intent.OPEN"))
-		{
-			String text = (String) intent.getExtras().get(KEY_PUSH_DATA);
-			String val = "";
-			try {
-				JSONObject pushjson = new JSONObject(text);
-				val = (String) pushjson.get("alert");
-			}
-			catch (Exception ex)
-			{
+			//Log.d("got a push", null);
+            ParseObject message = new ParseObject("messages");
+            String text = (String) intent.getExtras().get(KEY_PUSH_DATA);
+            try {
+                //JSONArray array = new JSONArray(text);
 
-			}
-			Uri uri = CommonUtils.findURIInText(val);
-			if(uri != null) {
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.setData(uri);
-				context.startActivity(i);
-			}
-			else {
+                JSONObject pushjson = new JSONObject(text);
 
-				Intent intent1 = new Intent(context, PushMessagesActivity.class);
-				intent1.putExtras(intent.getExtras());
-				intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				context.startActivity(intent1);
-			}
+                String val = (String) pushjson.get("alert");
+
+                message.put("text", val);
+
+
+                message.put("date",System.currentTimeMillis());
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+            message.pinInBackground();
+            context.sendBroadcast(new Intent("newMessage"));
 		}
+        else if(intent.getAction().equalsIgnoreCase("com.parse.push.intent.OPEN"))
+        {
+            String text = (String) intent.getExtras().get(KEY_PUSH_DATA);
+            String val = "";
+            try {
+                JSONObject pushjson = new JSONObject(text);
+                val = (String) pushjson.get("alert");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Uri uri = CommonUtils.findURIInText(val);
+            if(uri != null) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(uri);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            }
+            else {
+
+                Intent intent1 = new Intent(context, PushMessagesActivity.class);
+                intent1.putExtras(intent.getExtras());
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent1);
+            }
+        }
 		
 		
 		
 		
 	
 	}
-	
-	
-	
 }
